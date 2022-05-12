070627d37a4d1f54b8cdea56a76af617ddcc1b26
John Morgan <john.morgan@infernored.com>
2022-05-12 14:05:22 -0400

    ✨ Mint internal tokens

--- contracts/Token.sol
+++ contracts/Token.sol
@@ -2,14 +2,33 @@
 pragma solidity ^0.8.4;
 
 import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+import "@openzeppelin/contracts/utils/Counters.sol";
 
 import "../lib/Helpers.sol";
 
+uint256 constant maxTokensPerTransaction = 5;
+uint256 constant internalSupply = 100;
+address constant mintAddress = 0x697fB223656dEa2b0EFCCfc6FBFc1CE4E7224843;
+
 contract Token is ERC721 {
+    using Counters for Counters.Counter;
+
+    Counters.Counter private tokenIDs;
     string private baseUri;
 
-    constructor(string memory uri) ERC721("Token", "MTK") {
+    constructor(string memory uri) ERC721("Token", "IRT") {
+        tokenIDs.increment();
         baseUri = uri;
+
+        // TODO: move address to config
+        mintInternalTokens(mintAddress);
+    }
+
+    function mintInternalTokens(address to) internal {
+        for (uint256 i = 0; i < internalSupply; i++) {
+            _safeMint(to, tokenIDs.current());
+            tokenIDs.increment();
+        }
     }
 
     // Apparently we need to use View instead of pure here,
