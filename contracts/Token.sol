ab10dfd47bef39641af24feb13968d0a4e183db0
John Morgan <john.morgan@infernored.com>
2022-05-12 17:23:39 -0400

    ✨ mint tokens

--- contracts/Token.sol
+++ contracts/Token.sol
@@ -2,38 +2,83 @@
 pragma solidity ^0.8.4;
 
 import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+import "@openzeppelin/contracts/token/ERC721/extensions/IERC721Enumerable.sol";
 import "@openzeppelin/contracts/utils/Counters.sol";
 
 import "../lib/Helpers.sol";
 
-uint256 constant maxTokensPerTransaction = 5;
-uint256 constant internalSupply = 100;
-address constant mintAddress = 0x697fB223656dEa2b0EFCCfc6FBFc1CE4E7224843;
-
-contract Token is ERC721 {
+contract Token is ERC721, IERC721Enumerable {
     using Counters for Counters.Counter;
 
-    Counters.Counter private tokenIDs;
+    mapping(address => uint256[]) private ownerTokenIndexes;
+    mapping(uint256 => uint256) private tokenIndex;
+
+    uint256 private constant MAX_TOKENS_PER_TRANSACTION = 5;
+    address private constant INTERNAL_ADDRESS =
+        0x697fB223656dEa2b0EFCCfc6FBFc1CE4E7224843;
+    uint256 private constant INTERNAL_SUPPLY = 100;
+
+    Counters.Counter private supplyCounter;
     string private baseUri;
 
     constructor(string memory uri) ERC721("Token", "IRT") {
-        tokenIDs.increment();
         baseUri = uri;
 
         // TODO: move address to config
-        mintInternalTokens(mintAddress);
+        mintInternalTokens(INTERNAL_ADDRESS);
     }
 
     function mintInternalTokens(address to) internal {
-        for (uint256 i = 0; i < internalSupply; i++) {
-            _safeMint(to, tokenIDs.current());
-            tokenIDs.increment();
+        mintTokens(to, INTERNAL_SUPPLY);
+    }
+
+    function mint(address to, uint256 count) public {
+        require(count <= MAX_TOKENS_PER_TRANSACTION, "Too many tokens to mint");
+        mintTokens(to, count);
+    }
+
+    function mintTokens(address to, uint256 amount) internal {
+        for (uint256 i = 0; i < amount; i++) {
+            mintToken(to, totalSupply());
+            supplyCounter.increment();
         }
+    }
+
+    function mintToken(address to, uint256 tokenId) internal {
+        _safeMint(to, tokenId);
+        tokenIndex[tokenId] = tokenId;
+        ownerTokenIndexes[to].push(tokenId);
     }
 
     // Apparently we need to use View instead of pure here,
     // because we need to read from baseUri.
     function _baseURI() internal view override returns (string memory) {
         return Helpers.getUri(baseUri);
+    }
+
+    function tokenOfOwnerByIndex(address owner, uint256 index)
+        public
+        view
+        override
+        returns (uint256)
+    {
+        require(balanceOf(owner) > index, "index out of range");
+        return ownerTokenIndexes[owner][index];
+    }
+
+    function totalSupply() public view override returns (uint256) {
+        return supplyCounter.current();
+    }
+
+    // Assume no burning of tokens.
+    // QUESTION: Can you reject without a return?
+    function tokenByIndex(uint256 index)
+        external
+        view
+        override
+        returns (uint256)
+    {
+        require(index >= 0 && index <= totalSupply(), "invalid index");
+        return tokenIndex[index];
     }
 }
--- test/index.ts
+++ test/index.ts
@@ -1,7 +1,7 @@
 import { expect } from "chai";
 import { ethers } from "hardhat";
 
-describe("Test helper library", function () {
+describe("Test token contract", function () {
   before(async function () {
     this.tokenFactory = await ethers.getContractFactory("Token");
   });
@@ -13,5 +13,37 @@ describe("Test helper library", function () {
 
   it("Verify address", async function () {
     expect(42).to.equal(await this.token.address.length);
+  });
+
+  it("Reported total supply is accurate for initial supply", async function () {
+    expect(await this.token.totalSupply()).to.equal(100);
+  });
+
+  it("token by index retrieves correct token", async function () {
+    expect(await this.token.tokenByIndex(0)).to.equal(0);
+  });
+
+  // QUESTION: This runs very slow (677ms). Any optimizations?
+  it("tokenOfOwnerByIndex retrieves correct token", async function () {
+    const initialSupply = 100;
+    const address = this.token.ownerOf(0);
+    for (let i = 0; i < initialSupply; i++) {
+      expect(await this.token.tokenOfOwnerByIndex(address, i)).to.equal(i);
+    }
+  });
+
+  it("mintTokens mints tokens to owner's balance", async function () {
+    const tokensToMint = 5;
+    const owner = this.token.ownerOf(0);
+    const priorBalance = Number.parseInt(await this.token.balanceOf(owner));
+    const currentSupply = Number.parseInt(await this.token.totalSupply());
+    await this.token.mint(owner, tokensToMint);
+
+    expect(await this.token.totalSupply()).to.equal(
+      currentSupply + tokensToMint
+    );
+    expect(await this.token.balanceOf(owner)).to.equal(
+      priorBalance + tokensToMint
+    );
   });
 });
